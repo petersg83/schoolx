9dc83376b15fa42762eb0808aed1e1fe501841bf
Unknown <petersg83@gmail.com>
2019-03-10 01:16:33 +0100

    handle rateLimitExceeded error of gdrive

--- backend/scripts/backup.sh
+++ backend/scripts/backup.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-source ${0%/*}/config.sh
+SCRIPT_PATH=$(dirname $(realpath -s $0))
+source $SCRIPT_PATH/config.sh
 
 fileName=schoolx_`date +%Y-%m-%d_%H:%M`.bak
 
 mkdir -p ~/postgres/backups/schoolx/
 pg_dump schoolx > ~/postgres/backups/schoolx/$fileName
 gdrive upload ~/postgres/backups/schoolx/$fileName -p $directoryId > /dev/null
+result="not done yet"
+while [ -n "$result" ]; do
+  result=`gdrive upload ~/postgres/backups/schoolx/$fileName -p $directoryId | grep "rateLimitExceeded"`
+  sleep 5
+done
+
